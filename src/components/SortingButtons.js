9a2456f12e378ff13b91488e1b947aacbfb78829
+ <+>
2022-02-04 12:18:56 -0500

    Done.

--- src/components/SortingButtons.js
+++ src/components/SortingButtons.js
@@ -3,14 +3,17 @@ import React, { useState } from "react";
 function SortingButtons({ setHogsToFilter, hogsToFilter }) {
     const [removeGreased, setRemoveGreased] = useState(true)
     const unGreasedHogs = hogsToFilter.filter(hog => !hog.greased)
+    const [currentPigSet, setCurrentPigSet] = useState([...hogsToFilter])
 
     const toggleGreasedHogs = () => {
 
         if (removeGreased) {
             setHogsToFilter(unGreasedHogs)
             setRemoveGreased(false)
+            setCurrentPigSet(unGreasedHogs)
         } else {
             setHogsToFilter(hogsToFilter)
+            setCurrentPigSet(hogsToFilter)
             setRemoveGreased(true)
         }
     }
@@ -19,19 +22,21 @@ function SortingButtons({ setHogsToFilter, hogsToFilter }) {
         return x.name.localeCompare(y.name);
     }
 
-    const sortByName = (hogsToFilter) => {
-        let alphabetizedPigs = hogsToFilter.sort(SortArray);
-        // console.log(alphabetizedPigs);
+    const sortByName = () => {
+        let alphabetizedPigs = [...currentPigSet].sort(SortArray);
+
         setHogsToFilter(alphabetizedPigs)
     }
 
-
+    const sortByWeight = () => {
+        let weightSortedPigs = currentPigSet
+    }
 
     return (
         <div style={{ padding: '10px' }} >
             <button onClick={toggleGreasedHogs}>{removeGreased ? "Remove Greased Pigs" : "Restore Greased Pigs"}</button>
             <button onClick={sortByName}>Sort By Name</button>
-            <button>Sort by Weight</button>
+            <button onClick={sortByWeight}>Sort by Weight</button>
         </div>
     )
 
